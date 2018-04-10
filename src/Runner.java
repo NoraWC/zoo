e2b6728c3850dc9aad93a8acb5504d71f92c0c0c
Nora Coates <noracoates@students.berkeley.net>
2018-04-10 09:46:59 -0700

    java zoo oop update

--- src/Runner.java
+++ src/Runner.java
@@ -2,24 +2,24 @@ import java.util.ArrayList;
 public class Runner {
     public static void main(String[] args) {
         Bear pooh = new Bear("Pooh");
-//        pooh.eat("honey");
-//        pooh.eat("fish");
+        pooh.eat("honey");
+        pooh.eat("fish");
 
         Tiger tigger = new Tiger("Tigger");
-//        tigger.eat("bacon");
-//        tigger.eat("meat");
+        tigger.eat("bacon");
+        tigger.eat("meat");
 
         Unicorn rarity = new Unicorn("Rarity");
-//        rarity.eat("berries");
-//        rarity.eat("Marhsmallows");
+        rarity.eat("berries");
+        rarity.eat("Marhsmallows");
 
         Giraffe gemma = new Giraffe("Gemma");
-//        gemma.eat("leaves");
-//        gemma.eat("ham");
+        gemma.eat("leaves");
+        gemma.eat("ham");
 
         Bee stinger = new Bee("Stinger");
-//        stinger.eat("pollen");
-//        stinger.eat("pinecones");
+        stinger.eat("pollen");
+        stinger.eat("pinecones");
 
         ArrayList<Animal> animals = new ArrayList<Animal>(5);
         animals.add(tigger);
